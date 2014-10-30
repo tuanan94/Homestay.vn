732925dc70244814ea009a69e2032954a3c95d67
AnTTSE61258 <tuanan94@hotmail.com>
2014-10-30 21:00:44 +0700

    DAL 2

---
+++ TestDAL/TestDAL/TestDAL/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HomestayDAL;
+namespace TestDAL
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HouseManager HouseManager = new HouseManager();
+            
+            House hs = new House();
+            hs.Price=5;
+            hs.HouseScore = 0;
+            hs.Address = "sai gon";
+            hs.NORating = 0;
+            hs.UserID = 0;
+            hs.Detail = "sssssssssss";
+            HouseManager.House_Insert(hs);
+
+        }
+    }
+}
